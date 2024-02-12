0c24814e4c2c1351ab605917e158746f2b6fd5fb
Diter Evan <hansevangelista@gmail.com>
2024-02-11 19:57:21 -0500

    add changes

---
+++ src/pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+
+  <h1>
+    The most popular <br>
+    <!-- <span class="grad blur">component library</span> <br> -->
+    <span class="grad">component library</span> <br>
+    for Tailwind CSS
+  </h1>
+
+  <h2>
+    daisyUI adds component class names to Tailwind CSS <br>
+    so you can make beautiful websites faster than ever.
+  </h2>
+
+  <br>
+
+  <button>
+    How to use? <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3"></path></svg>
+  </button>
+
+</template>
